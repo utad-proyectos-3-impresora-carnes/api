4bf79aaebe8048854f444cee333a1162e04eec2e
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-02-14 19:49:34 +0100

    Finished user model

--- code/models/users.ts
+++ code/models/users.ts
@@ -5,23 +5,27 @@ import { Schema, model } from "mongoose";
  */
 const UserSchema = new Schema(
 	{
-		name: {
+		// El email.
+		email: {
 			type: String
 		},
-		age: {
-			type: Number
+		// Contraseña cifrada, marcada para que no se devuelva en los select.
+		password: {
+			type: String,
+			select: false
 		},
-		password:{
-			type:String,
-			select:false
+		// Teléfono del usuario. De momento no es usado pero será necesario para el doble factor de autenticación.
+		phone: {
+			type: String
 		}
 	},
 	{
+		// Guarda Los tiempos de creación.
 		timestamps: true,
 		versionKey: false
 	}
 
-);	
+);
 
 /**
  * Crear un modelo de mongoose.
