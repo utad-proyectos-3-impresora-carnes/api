ac06b0ac023dd33e721ecb556182ff30f30c1c2d
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-02-20 16:12:36 +0100

    Added interfaces

---
+++ code/constants/groupTypes.ts
@@ -0,0 +1,11 @@
+/**
+ * Tipos de grupos posibles.
+ */
+enum GroupTypes {
+	GRADO,
+	MASTER,
+	CICLO,
+	STAFF
+}
+
+export default GroupTypes;
---
+++ code/interfaces/group.ts
@@ -0,0 +1,24 @@
+import GroupTypes from "../constants/groupTypes";
+import MongoDBGenericObjectInterface from "./mongoGenericData";
+
+/**
+ * Interfaz de un objeto grupo.
+ */
+export default interface GroupInterface extends MongoDBGenericObjectInterface {
+
+	/**
+	 * Nombre del grupo
+	 */
+	name:string,
+
+	/**
+	 * Typo del grupo
+	 */
+	type?: GroupTypes,
+
+	/**
+	 * Año de creación del grupo
+	 */
+	creationYear?: number
+
+}
