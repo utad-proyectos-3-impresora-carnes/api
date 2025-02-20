d313f10b40522d8ae74dd7948c046c510dbf7c7b
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-02-20 09:36:07 +0100

    Added methods to ontroller

--- code/controllers/user.ts
+++ code/controllers/user.ts
@@ -9,27 +9,17 @@ import ExampleService from "../utils/example";
  * @param res Response
  * @returns La respuesta después de realizar las acciones.
  */
-async function exampleGet(req: any, res: any) {
+async function createUser(req: any, res: any) {
 
-	// Siempre debe haber try catch para que aunque falle la operación el servidor no se caiga.
 	try {
 
-		// Crea un nuevo objeto de servicio.
-		const exampleService = new ExampleService();
-
-		// Responde a la petición
-		return res.status(200).send({
-			text: "Thou arth seeing an example GET!",
-			fetchedData: exampleService.getUsefulArray()
-		});
 
 	} catch (error: any) {
 
+		console.error(error);
+
 		// Al menos poned un pequeño mensaje de ejemplo para que se sepa de donde proviene el error.
-		return res.status(500).send({
-			alert: "The example GET controller failed!",
-			error: error.message
-		});
+		return res.status(500).send("The example GET controller failed!");
 
 	}
 
@@ -44,30 +34,21 @@ async function exampleGet(req: any, res: any) {
  * @param res Response
  * @returns La respuesta después de realizar las acciones.
  */
-async function examplePost(req: any, res: any) {
+async function login(req: any, res: any) {
 
-	// Siempre debe haber try catch para que aunque falle la operación el servidor no se caiga.
 	try {
 
-		// Crea un nuevo objeto de servicio.
-		const exampleService = new ExampleService();
-
-		return res.status(200).send({
-			text: "Thou arth seeing an example POST!",
-			fetchedData: exampleService.getUsefulNumber()
-		});
 
 	} catch (error: any) {
 
+		console.error(error);
+
 		// Al menos poned un pequeño mensaje de ejemplo para que se sepa de donde proviene el error.
-		return res.status(500).send({
-			alert: "The example POST controller failed!",
-			error: error.message
-		});
+		return res.status(500).send("The example GET controller failed!");
 
 	}
-}
 
+}
 /**
  * Ejemplo de un controlador.
  * Las req y res no se pueden tipar, pero el resto si se debería. 'any' no clarifica los contenidos!
@@ -77,30 +58,21 @@ async function examplePost(req: any, res: any) {
  * @param res Response
  * @returns La respuesta después de realizar las acciones.
  */
-async function examplePut(req: any, res: any) {
+async function resetPassword(req: any, res: any) {
 
-	// Siempre debe haber try catch para que aunque falle la operación el servidor no se caiga.
 	try {
 
-		// Crea un nuevo objeto de servicio.
-		const exampleService = new ExampleService();
-
-		return res.status(200).send({
-			text: "Thou arth seeing an example PUT!",
-			fetchedData: exampleService.getUsefulString()
-		});
 
 	} catch (error: any) {
 
+		console.error(error);
+
 		// Al menos poned un pequeño mensaje de ejemplo para que se sepa de donde proviene el error.
-		return res.status(500).send({
-			alert: "The example PUT controller failed!",
-			error: error.message
-		});
+		return res.status(500).send("The example GET controller failed!");
 
 	}
-}
 
+}
 /**
  * Ejemplo de un controlador.
  * Las req y res no se pueden tipar, pero el resto si se debería. 'any' no clarifica los contenidos!
@@ -110,31 +82,21 @@ async function examplePut(req: any, res: any) {
  * @param res Response
  * @returns La respuesta después de realizar las acciones.
  */
-async function examplePatch(req: any, res: any) {
+async function getUserData(req: any, res: any) {
 
-	// Siempre debe haber try catch para que aunque falle la operación el servidor no se caiga.
 	try {
-
-		// Crea un nuevo objeto de servicio.
-		const exampleService = new ExampleService();
-
-		return res.status(200).send({
-			text: "Thou arth seeing an example PATCH!",
-			fetchedData: exampleService.getUsefulBool()
-		});
 
 
 	} catch (error: any) {
 
+		console.error(error);
+
 		// Al menos poned un pequeño mensaje de ejemplo para que se sepa de donde proviene el error.
-		return res.status(500).send({
-			alert: "The example PATCH controller failed!",
-			error: error.message
-		});
+		return res.status(500).send("The example GET controller failed!");
 
 	}
-}
 
+}
 /**
  * Ejemplo de un controlador.
  * Las req y res no se pueden tipar, pero el resto si se debería. 'any' no clarifica los contenidos!
@@ -144,34 +106,54 @@ async function examplePatch(req: any, res: any) {
  * @param res Response
  * @returns La respuesta después de realizar las acciones.
  */
-async function exampleDelete(req: any, res: any) {
+async function updateUser(req: any, res: any) {
 
-	// Siempre debe haber try catch para que aunque falle la operación el servidor no se caiga.
 	try {
 
-		throw new Error("Ejemplo de fallo de controlador");
-
-		return res.status(200).send("Thou arth seeing an example DELETE!");
 
 	} catch (error: any) {
 
+		console.error(error);
+
 		// Al menos poned un pequeño mensaje de ejemplo para que se sepa de donde proviene el error.
-		return res.status(500).send({
-			alert: "The example DELETE controller failed!",
-			error: error.message
-		});
+		return res.status(500).send("The example GET controller failed!");
 
 	}
-}
 
+}
+/**
+ * Ejemplo de un controlador.
+ * Las req y res no se pueden tipar, pero el resto si se debería. 'any' no clarifica los contenidos!
+ * Un controlador nunca puede llamar a otro controlador, solo a servicios!
+ * 
+ * @param req Request
+ * @param res Response
+ * @returns La respuesta después de realizar las acciones.
+ */
+async function deleteUser(req: any, res: any) {
+
+	try {
+
+
+	} catch (error: any) {
+
+		console.error(error);
+
+		// Al menos poned un pequeño mensaje de ejemplo para que se sepa de donde proviene el error.
+		return res.status(500).send("The example GET controller failed!");
+
+	}
+
+}
 /**
  * Añadir aquí abajo los controladores para que se exporten bien!
  * Se puede poner un export al principio de la función del controlador 'export function nombreFuncion' pero entonces no te autocompletará en el router con el objeto controldor.
  */
 export default {
-	exampleGet,
-	examplePost,
-	examplePut,
-	examplePatch,
-	exampleDelete
+	createUser,
+	login,
+	resetPassword,
+	getUserData,
+	updateUser,
+	deleteUser
 };
