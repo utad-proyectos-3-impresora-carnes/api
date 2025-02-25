ee8f72521c29f37758fe79ed0797ec21c260f94c
rafael99TW <124179747+rafael99TW@users.noreply.github.com>
2025-02-25 14:23:10 +0100

    Agregado funcion url

--- code/prototypes/card_generator/card.php
+++ code/prototypes/card_generator/card.php
@@ -1,7 +1,7 @@
 <?php
 // Paso 1: Conexión a la base de datos
 $host = "localhost"; // Cambia esto si es necesario
-$usuario = "root"; // Tu usuario de MySQL0
+$usuario = "root"; // Tu usuario de MySQL
 $contraseña = "1234"; // Tu contraseña de MySQL
 $base_de_datos = "alumnos";
 
@@ -12,9 +12,9 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-// Paso 2: Leer la imagen desde el ordenador
-$imagen_path = 'C:\Users\Rafael\Desktop\Foto - copia - copia.png'; // Cambia esto con la ruta completa de la imagen en tu ordenador
-$foto = file_get_contents($imagen_path); // Lee la imagen como datos binarios
+// Paso 2: Leer la imagen desde una URL
+$imagen_url = 'https://cdn.discordapp.com/attachments/1334909232117448785/1337048440760176690/Foto.png?ex=67be6aa5&is=67bd1925&hm=eb985878d7e904c35be4bee2a5c67421903955def75a220e16e19075d84d84e6&'; // Cambia esto con la URL de la imagen
+$foto = file_get_contents($imagen_url); // Lee la imagen desde la URL como datos binarios
 
 // Paso 3: Insertar los datos del alumno
 $nombre = 'Sofia Sánchez Fernández';
