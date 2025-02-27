f4bcff13211906ad92135b8caacf77dc0b8f6bf6
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-02-27 13:46:33 +0100

    Añadidos validadores de id

--- code/validators/member.ts
+++ code/validators/member.ts
@@ -1,23 +1,33 @@
-import { check } from "express-validator";
+import { param, query, body } from "express-validator";
 import validateResults from "../utils/validator";
 
-export const validatorCreateItem = [
-	check("name")
-		.exists().withMessage("Must exist!!!")
-		.notEmpty().withMessage("Can't be empty!!!")
-		.isEmail().withMessage("Must be an email!!!"), //.isLength(min:5, max:90)
-	check("album").exists().notEmpty(),
-	check("cover").exists().notEmpty(),
-	check("artist").exists().notEmpty(),
-	check("artist.name").exists().notEmpty(),
-	check("artist.nickname").exists().notEmpty(),
-	check("artist.nationality").exists().notEmpty(),
-	check("duration.start").exists().notEmpty().isInt(),
-	check("duration.end").exists().notEmpty().isInt(),
-	check("mediaId").exists().notEmpty().isMongoId(),
+// export const validatorCreateItem = [
+// 	check("name")
+// 		.exists().withMessage("Must exist!!!")
+// 		.notEmpty().withMessage("Can't be empty!!!")
+// 		.isEmail().withMessage("Must be an email!!!"), //.isLength(min:5, max:90)
+// 	check("album").exists().notEmpty(),
+// 	check("cover").exists().notEmpty(),
+// 	check("artist").exists().notEmpty(),
+// 	check("artist.name").exists().notEmpty(),
+// 	check("artist.nickname").exists().notEmpty(),
+// 	check("artist.nationality").exists().notEmpty(),
+// 	check("duration.start").exists().notEmpty().isInt(),
+// 	check("duration.end").exists().notEmpty().isInt(),
+// 	check("mediaId").exists().notEmpty().isMongoId(),
+// 	(req: any, res: any, next: any) => validateResults(req, res, next)
+// ]
+
+/**
+ * Checks if the memberId exists.
+ */
+export const checkMemberId = [
+
+	param("memberId")
+		.exists().withMessage("El id del miembro debe existir.")
+		.notEmpty().withMessage("El id del miembro no puede estar vacío.")
+		.isMongoId().withMessage("El id del miembro debe ser de mongoDB."),
+
 	(req: any, res: any, next: any) => validateResults(req, res, next)
-]
-export const validator = [
-	check("id").exists().notEmpty().isMongoId(),
-	(req: any, res: any, next: any) => validateResults(req, res, next)
+
 ]
---
+++ code/validators/user.ts
@@ -0,0 +1,33 @@
+import { param, query, body } from "express-validator";
+import validateResults from "../utils/validator";
+
+// export const validatorCreateItem = [
+// 	check("name")
+// 		.exists().withMessage("Must exist!!!")
+// 		.notEmpty().withMessage("Can't be empty!!!")
+// 		.isEmail().withMessage("Must be an email!!!"), //.isLength(min:5, max:90)
+// 	check("album").exists().notEmpty(),
+// 	check("cover").exists().notEmpty(),
+// 	check("artist").exists().notEmpty(),
+// 	check("artist.name").exists().notEmpty(),
+// 	check("artist.nickname").exists().notEmpty(),
+// 	check("artist.nationality").exists().notEmpty(),
+// 	check("duration.start").exists().notEmpty().isInt(),
+// 	check("duration.end").exists().notEmpty().isInt(),
+// 	check("mediaId").exists().notEmpty().isMongoId(),
+// 	(req: any, res: any, next: any) => validateResults(req, res, next)
+// ]
+
+/**
+ * Checks if the userId exists.
+ */
+export const checkUserId = [
+
+	param("userId")
+		.exists().withMessage("El id del usuario debe existir.")
+		.notEmpty().withMessage("El id del usuario no puede estar vacío.")
+		.isMongoId().withMessage("El id del usuario debe ser de mongoDB."),
+
+	(req: any, res: any, next: any) => validateResults(req, res, next)
+
+]
