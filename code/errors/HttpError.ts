348b7854b942d6d4c46cb1c277f2a558db3cc7a7
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-03-06 12:19:25 +0100

    Implemented http error

--- code/errors/HttpError.ts
+++ code/errors/HttpError.ts
@@ -1,9 +1,38 @@
+interface HttpErrorOptions {
+	res: any;
+	message?: string;
+	statusCode?: number;
+	lang?: string;
+}
+
+/**
+ * Class for an standars http error.
+ */
 export default class HttpError extends Error {
 
-	constructor(res: any, code: string, responseCode: number = 500) {
+	private readonly res: any;
+	private readonly statusCode: number;
 
-		super()
-	
+	/**
+	 * 
+	 * @param res The response object of the query
+	 * @param code The code of the error
+	 * @param statusCode The http status code to display to the client.
+	 */
+	constructor(options: HttpErrorOptions) {
+
+		super(options.message);
+
+		this.res = options.res;
+		this.statusCode = options.statusCode;
+
+	}
+
+	/**
+	 * Sends the response with the error.
+	 */
+	send() {
+		this.res.status(this.statusCode).send(this.message);
 	}
 
 }
